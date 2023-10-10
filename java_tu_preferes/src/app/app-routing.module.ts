03ca80738b515e370d847900d04fc3846851d32b
chloe <chlo.depit@epfedu.fr>
2023-10-10 13:37:29 +0200

    liaison 2 pages

--- java_tu_preferes/src/app/app-routing.module.ts
+++ java_tu_preferes/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PartieComponent} from "./partie/partie.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'partie', component: PartieComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- java_tu_preferes/src/app/menu/menu.component.ts
+++ java_tu_preferes/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-menu',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 pseudoUser!: string;
+
+//constructor(private router: Router){ }
+  ngOnInit():void{}
+  //onContinue(): void{
+  //this.router.navigateByUrl('partie');
+  //}
 
 onSubmitForm():void {
   console.log(this.pseudoUser);
