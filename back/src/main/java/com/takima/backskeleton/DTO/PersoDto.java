55ba1e6a2e707f0117842bf771bed58b4be24429
Aurelie <aurelie.vidal92@gmail.com>
2023-10-16 11:50:18 +0200

    adding deketed files

---
+++ back/src/main/java/com/takima/backskeleton/DTO/PersoDto.java
@@ -0,0 +1,10 @@
+package com.takima.backskeleton.DTO;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class PersoDto {
+    private String prenom;
+}
--- front/src/app/menu/menu.component.ts
+++ front/src/app/menu/menu.component.ts
@@ -1,29 +1,46 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
-
+import {PersoService} from "../services/perso.service";
+import {Perso} from "../models/Perso";
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit{
-pseudoUser!: string;
-nbrJoueur!:number;
-constructor(private router: Router, private activatedRoute: ActivatedRoute){ }
+  pseudoUser!: string;
+  nbrJoueur!:number;
+  listperso;
+  persos!: Perso [];
+
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private persoService: PersoService){
+    this.listperso =persoService.findAll();
+  }
+
+
 
 
   ngOnInit():void{
-  this.activatedRoute.params.subscribe(s=>{
-    this.nbrJoueur = s["nbrJoueur"]
-    }
-  )
+    this.activatedRoute.params.subscribe(s=>{
+        this.nbrJoueur = s["nbrJoueur"]
+      }
+    )
+
+    this.listperso.subscribe(
+      x => {
+        this.persos = x;
+        console.log(x)
+      }
+    );
   }
   onContinue(): void{
-  this.router.navigateByUrl('questions');
+    this.router.navigateByUrl('questions');
   }
 
-onSubmitForm():void {
-  console.log(this.pseudoUser);
-  console.log("c'est réussi"+this.nbrJoueur);
-}
+  onSubmitForm():void {
+    console.log(this.pseudoUser);
+    console.log("c'est réussi"+this.nbrJoueur);
+  }
+
 }
