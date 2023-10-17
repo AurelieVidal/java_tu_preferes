a9fd6acc7deaedde2a5345823d735c6c641adbe1
Antoine ROYER BOURNELLE <royerb.antoine@gmail.com>
2023-10-17 10:39:54 +0200

    debut implémentation du carousel

--- front/src/app/menu/menu.component.ts
+++ front/src/app/menu/menu.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, OnInit, NgModule} from '@angular/core';
-import {ActivatedRoute, Router, RouterOutlet} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router, RouterLink, RouterOutlet} from "@angular/router";
 import {FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule, FormArray} from '@angular/forms';
 import {AsyncPipe, NgClass, NgForOf, NgIf} from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
@@ -7,6 +7,12 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {PersoService} from "../services/perso.service";
 import {Perso} from "../models/Perso";
 import {Observable} from "rxjs";
+import {
+  CarouselComponent, CarouselControlComponent,
+  CarouselIndicatorsComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent
+} from "@coreui/angular";
 
 /**
  * @title Input with error messages
@@ -18,7 +24,7 @@ import {Observable} from "rxjs";
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, NgIf, RouterOutlet, NgForOf, NgClass, AsyncPipe],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, NgIf, RouterOutlet, NgForOf, NgClass, AsyncPipe, CarouselComponent, CarouselIndicatorsComponent, CarouselInnerComponent, CarouselItemComponent, CarouselControlComponent, RouterLink],
 
 })
 
@@ -35,17 +41,10 @@ export class MenuComponent implements OnInit{
   persos!: Perso [];
   perso_choisi!: bigint;
   protected readonly Object = Object;
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private persoService: PersoService){
     this.listperso =persoService.findAll();
-  }
-
-
-  counter() {
-    return new Array(parseInt(this.activatedRoute.snapshot.params["nbrJoueur"]))
-  }
-
-  onContinue(): void{
-    this.router.navigateByUrl('questions');
   }
 
   onSubmitForm():void {
@@ -53,8 +52,12 @@ export class MenuComponent implements OnInit{
     console.log("c'est réussi"+this.nbrJoueur);
   }
 
-  onItemChange($event: any): void {
-    console.log('Carousel onItemChange', $event);
+  counter() {
+    return new Array(parseInt(this.activatedRoute.snapshot.params["nbrJoueur"]))
+  }
+
+  onContinue(): void{
+    this.router.navigateByUrl('questions');
   }
 
   SelectImage(id: any): void {
@@ -65,7 +68,7 @@ export class MenuComponent implements OnInit{
   pseudoFormControl = new FormControl('', [Validators.required]);
 
   onSubmit() {
-    console.warn(this.userForm.value);
+    console.log(this.usersPseudo);
     console.log("c'est réussi "+ parseInt(this.activatedRoute.snapshot.params["nbrJoueur"]));
     console.log(this.usersPseudo.value)
   }
@@ -84,6 +87,14 @@ export class MenuComponent implements OnInit{
         console.log(x)
       }
     );
+
+    this.slides = [];
+
+    for (let i = 1; i <= 15; i++) {
+      this.slides[i - 1] = {
+        src: `./assets/images/${i}.jfif`,
+      };
+    }
 
   }
 
