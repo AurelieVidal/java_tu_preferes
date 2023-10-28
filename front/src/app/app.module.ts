edb8256810a068023e1cc1dba4944347c811bbbe
Aurelie <aurelie.vidal92@gmail.com>
2023-10-28 17:57:29 +0200

    resolve conflicts

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -18,13 +18,8 @@ import {MatSliderModule} from "@angular/material/slider";
 import { GestionCartesComponent } from './gestion-cartes/gestion-cartes.component';
 import { AddCardComponent } from './add-card/add-card.component';
 import { AddLiaisonComponent } from './add-liaison/add-liaison.component';
-<<<<<<< HEAD
 import { SliderComponent } from './slider/slider.component';
-
-
-=======
 import { ConfigComponent } from './config/config.component';
->>>>>>> 8a49b0f92bb0c5f73cf30d403ef1354c6aba224d
 
 @NgModule({
   declarations: [
@@ -35,11 +30,8 @@ import { ConfigComponent } from './config/config.component';
     GestionCartesComponent,
     AddCardComponent,
     AddLiaisonComponent,
-<<<<<<< HEAD
     SliderComponent,
-=======
     ConfigComponent,
->>>>>>> 8a49b0f92bb0c5f73cf30d403ef1354c6aba224d
     // QuestionsComponent
 
   ],
