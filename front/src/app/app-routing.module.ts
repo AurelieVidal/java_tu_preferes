5b0a417f9a85e8e6c27212ef92b58175a1cf7d4e
Antoine ROYER BOURNELLE <royerb.antoine@gmail.com>
2023-10-30 15:58:45 +0100

    moove nbr manche from config to page-acceuil

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ import {ScoresComponent} from "./scores/scores.component";
 
 const routes: Routes = [
   { path: 'partie', component: PartieComponent },
-  { path: 'menu/:nbrJoueur', component: MenuComponent},
+  // { path: 'menu/:nbrJoueur', component: MenuComponent},
   { path: 'menu/:nbrJoueur/:nbManche', component: MenuComponent},
   { path: 'questions', component: QuestionsComponent},
   { path: 'cartes', component: GestionCartesComponent},
